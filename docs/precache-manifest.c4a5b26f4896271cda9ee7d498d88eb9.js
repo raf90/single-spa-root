971d66bd283b6b98caa8ff2086a54bdd487bd63b
Richie Fornario <richardfornario@gmail.com>
2020-01-16 10:02:51 -0600

    add docs folder

---
+++ docs/precache-manifest.c4a5b26f4896271cda9ee7d498d88eb9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008acc17792039f7e2f27d16c56eb04e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40afb5eb5d7918003d8f",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "450d91e4113f091e051c",
+    "url": "/static/js/2.d5c3bc5a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d5c3bc5a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "40afb5eb5d7918003d8f",
+    "url": "/static/js/main.7ba91ea2.chunk.js"
+  },
+  {
+    "revision": "00a5a46fa84af9449317",
+    "url": "/static/js/runtime-main.760aee6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
